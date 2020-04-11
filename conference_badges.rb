24a43246fd8a6063f39378242d318aba63a34451
Elliot Blanchard <public@elliotblanchard.com>
2020-04-11 16:38:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badges = []
+  names.each {|name| badges << badge_maker(name)}
+  squares
+end
