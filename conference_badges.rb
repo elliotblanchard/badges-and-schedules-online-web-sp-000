3680148513066a0e8b8394aaa514a5a11fa93848
Elliot Blanchard <public@elliotblanchard.com>
2020-04-11 16:41:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,5 +6,15 @@ end
 def batch_badge_creator(names)
   badges = []
   names.each {|name| badges << badge_maker(name)}
-  squares
+  badges
+end
+
+def room_maker(name,room)
+  "Hello, _____! You'll be assigned to room _____!"
+end
+
+def assign_rooms(names)
+  room_assignments = []
+  names.each {|name| room_assignments << badge_maker(name)}
+  room_assignments
 end
